3b5f578057e3f589d3a5c9f307561343fe12dd4f
amithkashyap <amithkashyaprv@gmail.com>
2024-01-15 15:47:28 +0530

    added constants file

---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+export const NETFLIX_LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const USER_AVATAR =
+  "https://occ-0-2611-3663.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229";
+
+export const TMDB_API_KEY = "027969a376e9574371cf94e702c406e5";
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMjc5NjlhMzc2ZTk1NzQzNzFjZjk0ZTcwMmM0MDZlNSIsInN1YiI6IjY1YTNiMTExN2Q1ZjRiMDBjYmI3ZGZjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.S-wMuRVjdud2Odv5l2Taa5XkdZJCQqe_X_rp8u1ACGg",
+  },
+};
+
+export const MOVIE_IMG_URL = "https://image.tmdb.org/t/p/w780";
