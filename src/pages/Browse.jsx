78476593e991ea1d889a51a2107bc89a9d1b8938
amithkashyap <amithkashyaprv@gmail.com>
2024-01-15 15:55:23 +0530

    added browse page

---
+++ src/pages/Browse.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "../components/Header";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "../components/MainContainer";
+import SecondaryContainer from "../components/SecondaryContainer";
+import usePopularMovies from "../hooks/usePopularMovies";
+import useTopRatedMovies from "../hooks/useTopRatedMovies";
+
+const Browse = () => {
+  useNowPlayingMovies();
+  usePopularMovies();
+  useTopRatedMovies();
+
+  return (
+    <div>
+      <Header />
+      <MainContainer />
+      <SecondaryContainer />
+    </div>
+  );
+};
+
+export default Browse;
