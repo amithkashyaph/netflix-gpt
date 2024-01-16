bb3f1212252e60d4c8cedcc86428eaee82d3c811
amithkashyap <amithkashyaprv@gmail.com>
2024-01-16 16:14:43 +0530

    added languageConstants and updated existing constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,6 +4,8 @@ export const NETFLIX_LOGO =
 export const USER_AVATAR =
   "https://occ-0-2611-3663.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229";
 
+export const NETFLIX_BG_IMG =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/594f8025-139a-4a35-b58d-4ecf8fdc507c/d3c4e455-f0bf-4003-b7cd-511dda6da82a/IN-en-20240108-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 export const TMDB_API_KEY = "027969a376e9574371cf94e702c406e5";
 export const API_OPTIONS = {
   method: "GET",
@@ -15,3 +17,18 @@ export const API_OPTIONS = {
 };
 
 export const MOVIE_IMG_URL = "https://image.tmdb.org/t/p/w780";
+
+export const SUPPORTED_LANGUAGES = [
+  {
+    id: "en",
+    value: "English",
+  },
+  {
+    id: "ka",
+    value: "Kannada",
+  },
+  {
+    id: "hi",
+    value: "Hindi",
+  },
+];
---
+++ src/utils/languageConstants.js
@@ -0,0 +1,16 @@
+const languageConstants = {
+  en: {
+    search: "Search",
+    gptSearchPlaceHolder: "What would you like to watch today?",
+  },
+  ka: {
+    search: "ಹುಡುಕಿ",
+    gptSearchPlaceHolder: "ನೀವು ಇಂದು ಏನನ್ನು ವೀಕ್ಷಿಸಲು ಬಯಸುತ್ತೀರಿ?",
+  },
+  hi: {
+    search: "खोज",
+    gptSearchPlaceHolder: "आज आप क्या देखना चाहेंगे?",
+  },
+};
+
+export default languageConstants;
