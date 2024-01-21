d2cc27489def0fcac3acc91db8aa8a46388d5b38
amithkashyap <amithkashyaprv@gmail.com>
2024-01-21 16:20:40 +0530

    added Body component

---
+++ src/components/Body.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Login from "../pages/Login";
+import Browse from "../pages/Browse";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const Body = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
+
+  return (
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
+  );
+};
+
+export default Body;
