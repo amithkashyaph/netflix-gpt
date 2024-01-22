487728788028692cb9ccd8db78a74e880cb47dbd
amithkashyap <amithkashyaprv@gmail.com>
2024-01-22 23:23:17 +0530

    added main container

---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import VideoTitle from "./VideoTitle";
+import VideoBackground from "./VideoBackground";
+
+const MainContainer = () => {
+  const movies = useSelector((store) => store.movies?.nowPlayingMovies);
+
+  if (!movies) return;
+
+  const mainMovie = movies[13];
+
+  const { title, overview, id } = mainMovie;
+
+  return (
+    <div className="pt-[33%] md:pt-0 bg-black">
+      <VideoTitle title={title} overview={overview} />
+      <VideoBackground movieId={id} />
+    </div>
+  );
+};
+
+export default MainContainer;
